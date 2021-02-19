c36dabd9655376c6788776481b779459696d58dd
xuqian_HP_st <xuqian_engr@qq.com.com>
2021-02-19 08:13:47 +0800

    增加fuction3选项

--- main.py
+++ main.py
@@ -40,7 +40,22 @@ def writeFileTest():
     print("+      this modular for random basic group!   +")
     print("+      designed by rengrongfeng               +")
     print("+++++++++++++++++++++++++++++++++++++++++++++++")
-
+    data = []
+    for line in open('D:\data.txt'):
+        length = float(line.strip())
+        data.append(length)
+    n_items = len(data)
+    total = sum(data)
+    shortest = min(data)
+    longest = max(data)
+    data.sort()
+    output = open("D:\results.txt","w")
+    output.write("number of dendritic length:%4i \n" %(n_items))
+    output.write("total dendritic length    :%6.1f \n" %(total))
+    output.write("shortest dendritic length :%7.2f \n" %(shortest))
+    output.write("longest dendritic length  :%7.2f \n" %(longest))
+    output.write("%7.2f \n %7.2f" %(data[-2],data[-3]))
+    output.close()
 
 
 
@@ -50,12 +65,15 @@ def main():
     print("+       number 1 is get power                 +")
     print("+       number 2 is get random basic group    +")
     print("+++++++++++++++++++++++++++++++++++++++++++++++")
-    function = input("please select function(1-2):")
+    function = input("please select function(1-3):")
     string = "funtion number erro"
     if int(function) == 1:
         string = getPower()
     if int(function) == 2:
         string = getRandomBasicGroup(input("count:"))
+    if int(function) == 3:
+        writeFileTest()
+        string = "fuction 3 is done"
     print(string)
 
 if __name__ == '__main__':
